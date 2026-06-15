58d8c678c80fe34b7733bc9398f5660fa7ef56de
Ni, Tiffany <ni.tiffany130@gmail.com>
2026-06-15 18:01:38 -0400

    made User a JPA entity so we can use it for a database

--- src/main/java/com/shoopy/minesweeper/model/User.java
+++ src/main/java/com/shoopy/minesweeper/model/User.java
@@ -1,13 +1,37 @@
 package com.shoopy.minesweeper.model;
 
-//later turn this into an entity and add a repository for it so we can save users to the database
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String username;
     private String passwordHash;
+
+    // JPA requires a no-arg constructor
+    public User() {
+    }
 
     public User(String username, String passwordHash) {
         this.username = username;
         this.passwordHash = passwordHash;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getUsername() {
